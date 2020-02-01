a5afd17515ed1847b841b1d7fba1860255fe18ff
godonan <godonanabt@gmail.com>
2020-01-31 20:35:33 -0600

    Base Controls

---
+++ New Unity Project (1)/Assets/Scripts/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public float timeBetweenLoad = 0.50f;
+    private int remaining = 10;
+    private bool repairing;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        repairing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if(remaining > 0)
+        {
+            if (repairing == true)
+            {
+                if(Input.GetKey(KeyCode.E) && timer >= timeBetweenLoad)
+                { 
+                    timer = 0f;
+                    repairing = true;
+                    remaining -= 1;
+                }
+            }
+        }
+        if(remaining <= 0)
+        {
+            Debug.Log("GENERATOR DESTROYED!");
+        }
+    }
+}
