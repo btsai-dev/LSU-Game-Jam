353369bb3e1b428aaf7ce8b050f230c43c814c50
grnatkimblel <55123133+grnatkimblel@users.noreply.github.com>
2020-01-31 20:42:58 -0600

    Grant Pathfinding
    This is Enemy PathFinding Basics(Grants PC)

---
+++ New Unity Project (1)/Assets/EnemigoController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoController : MonoBehaviour
+{
+
+    public Camera cam;
+    public Vector3 position;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        position = GameObject.Find("FamilyMan").transform.position;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+            position = GameObject.Find("FamilyMan").transform.position;
+
+            agent.destination = position;
+            
+        
+    }
+}
